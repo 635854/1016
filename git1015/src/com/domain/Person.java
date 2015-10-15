3b94a70a9fd8347eff6a859f1faf46683094dc91
Administrator <Administrator@PC-201409010112>
2015-10-15 15:12:43 +0800

    Person.java

---
+++ git1015/src/com/domain/Person.java
@@ -0,0 +1,27 @@
+package com.domain;
+
+public class Person {
+	
+	private Integer pid;
+	
+	private String pname;
+
+	public Integer getPid() {
+		return pid;
+	}
+
+	public void setPid(Integer pid) {
+		this.pid = pid;
+	}
+
+	public String getPname() {
+		return pname;
+	}
+
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+	
+	
+
+}
